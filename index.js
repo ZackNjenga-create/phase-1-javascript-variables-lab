c904f75bd39c474e86842c6a5cd021815ef7e62c
Zack Njenga <izackkiari@gmail.com>
2025-03-22 17:27:40 +0300

    variable lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+let companyName = "Scuber";
+
+// const companyName = "Scubber";
+
+mostProfitableNeighborhood === "Chelsea"
+
+let mostProfitableNeighborhood = "Chelsea"
+
+companyCeo = "Susan Smith"
+
+let companyCeo = "Susan Smith"
+
